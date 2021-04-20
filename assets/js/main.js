e40d076ed8e7706d49b53c6b0aa1916ed183bf40
Panihe <57388777+Panihe@users.noreply.github.com>
2021-04-20 12:50:54 +0200

    Subiendo Actialización de los ficheros

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,10 @@
 /* General */
 const mainBox = document.getElementById('mainbox');
 const mainBox2 = document.getElementById('mainbox2');
+const mainBox3 = document.getElementById('mainbox3');
+/* const btn = document.getElementsByTagName('button'); */
+const boxBtnNext = document.getElementById('box-next');
 const btnNext = document.getElementById('next');
-const btn = document.getElementsByTagName('button');
-btnNext.addEventListener('click', () => continued());
 
 /* Block 1 */
 const btnMacedonia = document.getElementById('macedonia');
@@ -11,13 +12,10 @@ const btnKirguizistan = document.getElementById('kirguizistan');
 const btnSpain = document.getElementById('spain');
 const btnVietnam = document.getElementById('vietnam');
 
-
 btnMacedonia.addEventListener('click', () => changeColorBtnMacedonia());
 btnKirguizistan.addEventListener('click', () => changeColorBtnKirguizistan());
 btnSpain.addEventListener('click', () => changeColorBtnSpain());
-btnSpain.addEventListener('click', () => showBtnNext());
 btnVietnam.addEventListener('click', () => changeColorBtnVietnam());
-
 
 changeColorBtnMacedonia = () => {
     btnMacedonia.style.backgroundColor = "#c9302c";
@@ -38,25 +36,6 @@ changeColorBtnVietnam = () => {
     btnVietnam.style.backgroundColor = "#c9302c";
     btnVietnam.style.color = "#ffffff";
 }
-
-/* function continue with different id */
-let i = 1;
-continued = () => {
-    switch(i + 1){
-        case 2:
-            mainBox2.style.display = "block";
-            mainBox.style.display = "none";
-            i++;
-        break;
-    }
-}
-
-showBtnNext = () => {
-    btnNext.style.display = "block";
-}
-
-
-
 
 /* The function is missing so that when the user fails the correct option is displayed. */
 
@@ -93,5 +72,28 @@ changeColorBtnUganda = () => {
 
 /* The function is missing so that when the user fails the correct option is displayed. */
 
+/* function continue with different id */
+let i = 1;
+continued = () => {
+    switch(i + 1){
+        case 2:
+            mainBox2.style.display = "block";
+            mainBox.style.display = "none";
+            i++;
+        break;
+        case 3:
+            mainBox3.style.display = "block";
+            mainBox2.style.display = "none";
+            i++;
+        break;
+    }
+}
 
+showBtnNext = () => {
+    boxBtnNext.style.display = "flex";
+    boxBtnNext.style.justifyContent = "center";
+}
 
+btnNext.addEventListener('click', () => continued());
+btnSpain.addEventListener('click', () => showBtnNext());
+btnUganda.addEventListener('click', () => showBtnNext());
